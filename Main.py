5e38dcbc100ffb833235c033151b2e00e635c744
Peter <szpeto12@gmail.com>
2025-06-06 19:05:49 +0200

    Version v0.2.4

--- Main.py
+++ Main.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from functions import Functions
 
-# App setup
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Databases/database.db" # configure Flask to use a specific database
+def main():
+    # Master
+    functions = Functions()
+    functions.create_file("Log\\log.txt")
+    functions.create_dir("Databases")
 
-# Creating routes to webpages
-@app.route("/")
-def index():
-    return render_template("index.html")
+    # App setup
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Databases/database.db" # configure Flask to use a specific database
+
+    # Creating routes to webpages
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    app.run(debug=True)
 
 # Main
 if __name__ == "__main__":
-    app.run(debug = True)
+    main()
--- functions.py
+++ functions.py
@@ -6,6 +6,16 @@ class Functions:
 
     def __init__(self):
         self.log_file = self.create_file("Log\\log.txt")
+        self.first_log = True
+        self.write_log("****************************************************************************************")
+
+    def create_dir(self, dir_name):
+        try:
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            self.write_log(f"Directory {dir_name} successfully created")
+        except Exception as e:
+            self.write_log(f"def create_dir : {e}")
 
     def create_file(self, path_and_file):
 
@@ -13,11 +23,19 @@ class Functions:
             dir_name = os.path.dirname(path_and_file)
             if dir_name and not os.path.exists(dir_name):
                 os.makedirs(dir_name)
+                if os.path.exists("Log\\log.txt"):
+                    self.write_log(f"Directory {dir_name} successfully created")
         except Exception as e:
             print(f"Something went wrong creating the file or directory : {e}")
 
         return path_and_file
 
     def write_log(self, text):
-        with open(self.log_file, "a", encoding = "utf-8") as log_file:
-            timestamp = datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S - ")
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d, %H:%M:%S - ")
+        if self.first_log:
+            with open(self.log_file, "a", encoding="utf-8") as log_file:
+                log_file.write(f"\n{timestamp}{text}\n")
+                self.first_log = False
+        else:
+            with open(self.log_file, "a", encoding = "utf-8") as log_file:
+                log_file.write(f"{timestamp}{text}\n")
